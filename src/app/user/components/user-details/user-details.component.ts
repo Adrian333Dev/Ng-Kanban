956f70ad8067d5abad3eec5fef35753688b35ebf
Adrian333Dev <adrian333dev@gmail.com>
2023-07-27 11:48:35 +0400

    User details page canceled

--- src/app/user/components/user-details/user-details.component.ts
+++ src/app/user/components/user-details/user-details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { IUser } from '../../models/user.types';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit, OnDestroy {
+  public user: IUser;
+  private unSub = new Subject<void>();
 
+  // Get user id from route
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    // Get user id from route then get user from service
+    // this.route.params
+    //   .pipe(takeUntil(this.unSub))
+    //   .subscribe((params) =>
+    //     this.userService
+    //       .get(params['id'])
+    //       .subscribe((user) => (this.user = user))
+    //   );
+  }
+
+  ngOnDestroy(): void {
+    this.unSub.next();
+    this.unSub.complete();
+  }
 }
